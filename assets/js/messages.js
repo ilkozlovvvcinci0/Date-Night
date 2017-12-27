e37a2a1bd4f172013d66c1f15af4e6e3c0b4caad
Brady Jones <brady.jones@hotmail.co.uk>
2017-12-27 19:49:48 +0000

    added sender to the message display message list

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-  let message = localStorage.getItem("message_for_" + getUsername());
-  let body = $(".mes-main-body");
+	let message = localStorage.getItem("message_for_" + getUsername());
+  let msgList = $(".msg-list");
   let messages = JSON.parse(message);
-  for (let i = 0; i < messages.length; i++) {
-    body.append(`<div>${messages[i]}</div>`);
-  }
+	for (let i = 0; i < messages.length; i++) {
+    msgList.append(`
+      <div class="mes-par"> 
+        <div class="mess"> <p class="mess-head"> ${messages[i]} </p> </div>
+      </div>`)
+	}
 });
+
--- assets/js/send-message.js
+++ assets/js/send-message.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("#send-message").click(function() {
 		let sendMessagesKey = "message_for_" + getRecipient();
 		let messages = JSON.parse(localStorage.getItem(sendMessagesKey));
-		messages.push($("#text-area").val());
+		messages.push(`<div>From: ${getUsername()}</div>` + $("#text-area").val());
 		localStorage.setItem(sendMessagesKey, JSON.stringify(messages));
 	});
 });
@@ -13,7 +13,13 @@ function getRecipient() {
 }
 
 function goMessages() {
-  window.location.href = "messages.html?username=" + getUsername();
+  window.location.href = "messages.html?username=" +  getSender() + getUsername();
 }
+
+function getSender() {
+	let urlParams = new URLSearchParams(window.location.search);
+	return urlParams.get("username");
+}
+
 
 
