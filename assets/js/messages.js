c2c63226006c98b0b11bd778c1ae5a078559d410
Brady Jones <brady.jones@hotmail.co.uk>
2017-12-29 15:25:32 +0000

    added some space between sender and actual message

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   let msgList = $(".msg-list");
   let messages = JSON.parse(message);
 	for (let i = 0; i < messages.length; i++) {
-    msgList.append(`
-      <div class="mes-par"> 
-        <div class="mess"> <p class="mess-head"> ${messages[i]} </p> </div>
+    msgList.append (`
+      <div class="msg-parent"> 
+        <div class="msg"> ${messages[i]}  </div>
       </div>`)
 	}
 });
--- assets/js/send-message.js
+++ assets/js/send-message.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("#send-message").click(function() {
 		let sendMessagesKey = "message_for_" + getRecipient();
 		let messages = JSON.parse(localStorage.getItem(sendMessagesKey));
-		messages.push(`<div>From: ${getUsername()}</div>` + $("#text-area").val());
+		messages.push(`<div><div style=\"font-style: italic;\">from: ${getUsername()}</div></div>` + "&nbsp" + "-" + "&nbsp" + "&nbsp" + "&nbsp" + $("#text-area").val());
 		localStorage.setItem(sendMessagesKey, JSON.stringify(messages));
 	});
 });
@@ -13,13 +13,9 @@ function getRecipient() {
 }
 
 function goMessages() {
-  window.location.href = "messages.html?username=" +  getSender() + getUsername();
+  window.location.href = "messages.html?username=" + getUsername();
 }
 
-function getSender() {
-	let urlParams = new URLSearchParams(window.location.search);
-	return urlParams.get("username");
-}
 
 
 
