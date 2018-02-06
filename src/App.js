56b9e515557ffbfd9cce51138a79c3e22011b0d5
Brady Jones <brady.jones@hotmail.co.uk>
2018-02-06 15:21:31 +0000

    homepage completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="btn-con">
+          <button className="sign-up"> 
+            <li><a href="sign-up.html"> Sign Up </a></li>
+          </button>
+
+          <button className="login"> 
+            <li><a href="login.html"> Login </a></li>
+          </button>
+        </div>
+        <div className="date-night"> Date Night </div>
       </div>
     );
   }
