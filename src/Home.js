b1da03a2ebfccbde2092ff06af82cae4f135e98a
Brady Jones <brady.jones@hotmail.co.uk>
2018-02-13 15:14:34 +0000

    added Router

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Home.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="btn-con">
+          <button className="sign-up"> 
+            <li><Link to="/sign-up"> Sign Up </Link></li>
+          </button>
+
+          <button className="login"> 
+            <li><Link to="/login"> Login </Link></li>
+          </button>
+        </div>
+        <div className="date-night"> Date Night </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/Login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="login"> 
+      	<div className="log-user-con">
+
+
+
+
+
+      	</div>
+
+      </div>
+    );
+  }
+}
+
+export default Login;
