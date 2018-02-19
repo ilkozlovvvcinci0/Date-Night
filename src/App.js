a61192fa75ae29cb8e82fd325c90d324a3022a1f
Brady Jones <brady.jones@hotmail.co.uk>
2018-02-19 08:58:32 +0000

    created profile page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Home from './Home'
 import './App.css'; 
+import Checks from './Checks'; 
 import SignUp from './Sign-Up'; 
 import Login from './Login'; 
+import ProfilePage from './ProfilePage'; 
 import {
   BrowserRouter as Router,
-  Route,
-  Link
+  Route
 } from 'react-router-dom'
 
 class App extends Component {
@@ -20,8 +21,10 @@ class App extends Component {
       <Router>
         <div className="app">
           <Route exact path="/" component={Home}/>
-          <Route exact path="/sign-up" component={SignUp}/>}
-          <Route exact path="/login" component={Login}/>}
+          <Route exact path="/checks" component={Checks}/>
+          <Route exact path="/sign-up" component={SignUp}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/profile-page" component={ProfilePage}/>
         </div>
       </Router>
     );
--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,94 @@
 import React, { Component } from 'react';
 import './Login.css';
+{/*import Checks from './Checks';*/}
 
 class Login extends Component {
+  constructor(props) {
+  	super(props);
+	    this.state = {
+	    	username:"",
+	    	password:"",
+	    };
+
+	  this.handleUserChange = this.handleUserChange.bind(this);
+	  this.handlePassChange = this.handlePassChange.bind(this);
+	  this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+  handleUserChange(event) {
+    this.setState({username: event.target.value});
+  }
+
+  handlePassChange(event) {
+    this.setState({password: event.target.value});
+  }
+
+  handleSubmit(event) {
+
+    function checkUsername(username) {
+	    if(username.search(/[a-zA-Z]/) === -1) {
+	      console.log("Please fill out username field! Needs at least 1 letter.");
+	      return false;
+	   	}
+	   	return true;
+		}
+
+		function checkPassword(password) {
+	   if(password.length === 0 ) {
+	      console.log("A password is required!");
+	      return false;
+	   }
+	   
+	   if(password.length < 8) {
+	      console.log("Password needs to have at least 8 charcters!");
+	      return false;
+	   }
+
+	   if(password.search(/\d/) === -1) {
+	      console.log("Password needs to have a number!");
+	      return false;
+	   }
+
+	   if(password.search(/[a-zA-Z]/) === -1) {
+	      console.log("Password needs to have a letter!");
+	      return false;
+	   }
+
+	   return true;
+	}
+
+		checkUsername(this.state.username);
+		checkPassword(this.state.password);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="login"> 
-      	<div className="log-user-con">
+      	<form className="login-form" onSubmit={this.handleSubmit}>
+      		<div className="log-user-pass-con">
+		        <label>
+		          Username
+		          <input className="log-pass-user" type="text" onChange={this.handleUserChange} /> 
+		        </label>
+      		</div>
 
+      		<div className="log-user-pass-con">
+      			<label>
+		          Password
+		          <input className="log-pass-user" type="text" onChange={this.handlePassChange} /> 
+		        </label>
+      		</div>
 
-
-
-
-      	</div>
-
+      		<div className="log-btn-con">
+         		<div className="log-err-mes" id="errorMessageLogin"></div>
+		        <input type="submit" value="Login" className="login-btn" />
+		      </div>
+		    </form>
       </div>
     );
   }
 }
 
 export default Login;
+
