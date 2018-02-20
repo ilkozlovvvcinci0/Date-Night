2f80f2aa895abc1d8bb155ef809e917a9bb74412
Brady Jones <brady.jones@hotmail.co.uk>
2018-02-20 15:42:12 +0000

    added 'get swiping' button

---
+++ src/Checks.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Checks extends Component {
+  constructor(props) {
+  	super(props);
+    	this.state = {};
+	    
+	
+	}
+
+  render() {
+    return (
+      <div className="checks"></div>
+    
+
+
+
+
+
+
+    );
+  }
+}
+
+export default Checks;
+
---
+++ src/ProfilePage.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './ProfilePage.css'; 
+let FontAwesome = require('react-fontawesome');
+
+class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        FontAwesome
+      };
+      
+  
+  }
+
+  render() {
+    return (
+      <div className="profile-main-con">
+        <div className="profile-header">
+          
+          <div className="profile-icon"> 
+            <div> Settings </div>
+          </div>
+
+          <div className="profile-icon">  
+            <FontAwesome
+              className='super-crazy-colors'
+              name='rocket'
+              size='2x'
+            /> 
+          </div>
+
+          <div className="profile-icon">                       
+            <div> Edit Profile </div>
+          </div>
+
+        </div>
+
+        <div className="profile-body">
+
+          <div className="img-mes">
+            <div id="hiMessage"></div> 
+            <div className="profile-pic-container">
+              <img src={ require('./img/brady.jpg') } className="profile-pic" id="display-pic" />
+            </div>
+          </div>
+        </div>
+
+        <div className="profile-footer">
+          <input type="submit" value="Get Swiping" className="get-swiping-btn" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProfilePage;
+
+
