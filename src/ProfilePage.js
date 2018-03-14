88f8772cc33a14069b4702d52ad86615741a8ef8
Brady Jones <brady.jones@hotmail.co.uk>
2018-03-14 15:40:14 +0000

    importing getUserImage function

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import './ProfilePage.css'; 
 import FontAwesome from 'react-fontawesome'
 import faStyles from 'font-awesome/css/font-awesome.css';
-import {getUserImage} from './Users.js'
+import {getUserImage} from './Users.js';
 
 class ProfilePage extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        profilePic: `assets/img/${getUserImage(this.state.username)}`
+        profilePic: "assets/img/" + getUserImage(this.state.username)
       };
   }
+
 
   render() {
     return (
