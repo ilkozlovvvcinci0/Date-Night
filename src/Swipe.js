c532f92a9b73c11cd72c396e7ddeb48df2f01860
Brady Jones <brady.jones@hotmail.co.uk>
2018-03-21 11:09:41 +0000

    added properties from advice of https://github.com/akiran/react-slick

--- src/Swipe.js
+++ src/Swipe.js
@@ -2,30 +2,37 @@ import React, {Component} from "react";
 import "./Swipe.css"; 
 import FontAwesome from "react-fontawesome"
 import faStyles from "font-awesome/css/font-awesome.css";
-import {getUserImage, getUsername} from "./Users.js";
 import Carousel from 'nuka-carousel';
 
 class Swipe extends Component {
   constructor(props) {
     super(props);
-    let username = getUsername();
-    this.state = {
-      username: username,
-      profilePic: `assets/img/${getUserImage(username)}`
-    };
-  }
+  
+  };
   
   render() {
     return (
-      <Carousel>
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
-      </Carousel>
+      <div className="swipe-container">  
+        <div className="swipe-body">      
+
+          <Carousel>
+            <div className="profile-pic-container">
+              <div className="i-btn-con">
+                <button className="i-btn" id="Fran3" onclick="vup('Fran3')"> i </button>
+              </div>
+              <img src="assets/img/francesca.jpg" className="profile-pic" id="display-pic" data-username="Fran3" />
+            </div>
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+          </Carousel>
+        
     
+    
+        </div>
+      </div>
     );
   }
 }
