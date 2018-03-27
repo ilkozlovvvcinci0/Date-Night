5c405feea63a002f9c198c6c39c771272650f7fc
Brady Jones <brady.jones@hotmail.co.uk>
2018-03-27 11:40:36 +0100

    added vup pages

---
+++ src/ViewUserProfile.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import "./ViewUserProfile.css"; 
+
+class ViewUserProfile extends Component {
+  constructor(props) {
+    super(props);
+  
+  };
+
+  render() {
+    return (
+      <div className="">  </div>
+    );
+  }
+}
+
+export default ViewUserProfile;
