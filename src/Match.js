bf50a45523139db8fe891b7379f7c2891ecb7b21
Brady Jones <brady.jones@hotmail.co.uk>
2018-05-03 12:27:45 +0100

    changed main containers' and other necessary elements' heights to fit new root divs height

--- src/Match.js
+++ src/Match.js
@@ -18,26 +18,28 @@ class Match extends React.Component {
     const {user} = this.props.match.params
     return (
      <div className="match-container">
-      
-      <div className="match-header">
-        <div className="match-msg"> Hey, it's a match! </div>
-      </div>
-      
-      <div className="match-body">
-        <div className="match-pics">
-          <div className="match-pic-container"> 
-            <img src={this.state.profilePic} className="profile-pic" alt="" />
-          </div>
-          <div className="match-pic-container"> 
-            <img src={(`../assets/img/${getUserImage(user)}`)} className="profile-pic" alt="" />
+
+      <div className="match-elements">
+        <div className="match-header">
+          <div className="match-msg"> Hey, it's a match! </div>
+        </div>
+        
+        <div className="match-body">
+          <div className="match-pics">
+            <div className="match-pic-container"> 
+              <img src={this.state.profilePic} className="profile-pic" alt="" />
+            </div>
+            <div className="match-pic-container"> 
+              <img src={(`../assets/img/${getUserImage(user)}`)} className="profile-pic" alt="" />
+            </div>
           </div>
         </div>
-      </div>
-    
-      <div className="match-footer">
-        <div className="match-buttons">
-          <Link to="/swipe" className="keep-swiping"> Keep Swiping </Link>
-          <button className="send-message"> Send Message </button>
+      
+        <div className="match-footer">
+          <div className="match-buttons">
+            <Link to="/swipe" className="keep-swiping"> Keep Swiping </Link>
+            <button className="send-message"> Send Message </button>
+          </div>
         </div>
       </div>
     
