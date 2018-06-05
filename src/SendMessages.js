9095ea0912f5f3d3d49a1490f40d7a07212d82b8
Brady Jones <brady.jones@hotmail.co.uk>
2018-06-05 15:16:48 +0100

    Revert "Revert "added these new pages""
    This reverts commit b00a5ff11134dfaabb9acb230dfec27d69b1ad2a.

---
+++ src/SendMessages.js
@@ -0,0 +1,62 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import "./SendMessages.css"; 
+
+
+class SendMessages extends Component {
+  constructor(props) {
+    super(props);
+  
+
+  };
+
+  render() {
+    
+
+  
+    return (
+		  <div className="sm-con">
+				
+				<div className="sm-main-body">
+	        <div className="s-msg-con">
+	          <Link to="/messages" className="messages" onclick="goMessages()"> Messages </Link>
+	        </div>
+	        <div className="ta-btn">
+	          <textarea className="text-area" rows="15" cols="100"></textarea>
+	          <div className="btn-send"><button className="send"> Send </button></div>
+	        </div>
+	      </div>
+
+	      <div className="sm-main-footer">
+	        <div className="footer-btns">
+	          <Link to="/swipe" className="keep-swiping" onclick="goSwipe()"> Keep Swiping </Link>
+	          <button className="view-profile" onclick="vup(getMatchUsername())"> View Profile </button>
+	        </div>
+	      </div>
+
+    	</div>
+    );
+  }
+}
+
+export default SendMessages;
+
+    
+
+
+
+
+
+
+
+
+        
+  
+
+		 
+			  	
+   
+
+
+
+
