267c972758339e634ebfb405ac86b19b94d8e657
Brady Jones <brady.jones@hotmail.co.uk>
2018-06-07 17:14:15 +0100

    added messages page

---
+++ src/Messages.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import "./Messages.css"; 
+
+
+class Messages extends Component {
+  constructor(props) {
+    super(props);
+  
+
+  };
+
+  render() {
+    
+
+  
+    return (
+		  <div> </div>
+    );
+  }
+}
+
+export default Messages;
+
+    
+
+
+
+
+
+
+
+
+        
+  
+
+		 
+			  	
+   
+
+
+
+
